6d24e70a9face498de8c9366ae358ad0e05f1753
Parth <108926206+Parthkoringa@users.noreply.github.com>
2025-07-05 00:13:03 +0530

    initial test cases

--- src/main/java/org/Incubyte/StringCalculator.java
+++ src/main/java/org/Incubyte/StringCalculator.java
@@ -1,5 +1,10 @@
 package org.Incubyte;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringCalculator
 {
     public static void main( String[] args )
@@ -9,8 +14,22 @@ public class StringCalculator
         System.out.println(add(emptyString));
     }
 
-    static String add(String number)
+    static int add(String number)
     {
-        return null;
+        int sum = 0;
+        if (number.isEmpty())
+        {
+            return 0;
+        }
+        List<Integer> numberList = new ArrayList<Integer>();
+        numberList = Arrays.stream(number.split(","))
+                     .map(Integer::parseInt)
+                     .collect(Collectors.toList());
+
+        for(Integer i : numberList)
+        {
+            sum += i;
+        }
+        return sum;
     }
 }
--- src/test/java/org/Incubyte/StringCalculatorTest.java
+++ src/test/java/org/Incubyte/StringCalculatorTest.java
@@ -9,8 +9,26 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest
 {
     @Test
-    public void EmptyStringShouldReturnZero()
+    public void emptyString_ShouldReturnZero()
     {
-        assertEquals("0",StringCalculator.add("") );
+        assertEquals(0,StringCalculator.add("") );
+    }
+
+    @Test
+    public void singleNumber_ShouldReturnItSelf()
+    {
+        assertEquals(21,StringCalculator.add("21"));
+    }
+
+    @Test
+    public void multipleNumbersSeperatedByComa_ShouldReturnSumOfTheNumbers()
+    {
+        assertEquals(6,StringCalculator.add("1,2,3"));
+    }
+
+    @Test
+    public void numbersWithNewlineDelimiter_ShouldReturnSumOfTheNumbers()
+    {
+        assertEquals(10,StringCalculator.add("1\n2,7"));
     }
 }
